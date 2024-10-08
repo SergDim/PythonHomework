18f2a97df41dc767ffd3fcdbaefd1f50af29ff59
SergDim <sergdimplay@gmail.com>
2024-10-08 19:20:46 +0200

    Init

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = int(input())
+second = int(input())
+third = int(input())
+
+if first == second and third == second :
+    print(3)
+elif first == second or third == second or first == third :
+    print(2)
+else:
+    print(0)
